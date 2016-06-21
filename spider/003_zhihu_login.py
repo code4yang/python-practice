3da66c1590dd0c43ce460095b3dc4633e74b029b
adam <yang704298867@gmail.com>
2016-06-22 00:46:58 +0800

    add 003_zhihu_login but not finished

---
+++ spider/003_zhihu_login.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def login_zhihu():
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
+                        '(KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'}
+    session = requests.Session()
+    resp = session.get('http://www.zhihu.com/#signin', headers=header)
+    xsrf = BeautifulSoup(resp.content, 'lxml').find('input', attrs={'name': '_xsrf'}).attrs['value']
+    data = {'_xsrf': xsrf, 'password': '1234560.', 'captcha_type': 'cn', 'remember_me': 'true', 'email': '704298867@qq.com'}
+    print(data)
+    resp = session.post('http://www.zhihu.com/login/email', data=data, headers=header)
+    print(resp.json())
+
+login_zhihu()
